c973e87dc7c94e1441ca10e71085c969c75d7f65
Jonathon Hinchley <dev.jjh@planet-hinchley.com>
2021-07-23 11:27:35 -0500

    refactor to class methods and members player movement

--- src/game.ts
+++ src/game.ts
@@ -2,11 +2,14 @@ import 'phaser';
 
 export default class Demo extends Phaser.Scene
 {
+    player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    platforms: Phaser.Physics.Arcade.StaticGroup;
     constructor ()
     {
         super('demo');
     }
 
+    
     preload ()
     {
         this.load.image('sky', 'assets/sky.png');
@@ -23,30 +26,19 @@ export default class Demo extends Phaser.Scene
     //set up ground platforms using physics static group: can't move around
     addPlatforms()
     {
-        let platforms = this.physics.add.staticGroup();
-        platforms.create(400, 568, 'ground')
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 568, 'ground')
             .setScale(2)
             .refreshBody();
-        platforms.create(600, 400, 'ground');
-        platforms.create(50, 250, 'ground');
-        platforms.create(740,220, 'ground');
-        return platforms;
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(740,220, 'ground');
+        this.platforms;
     }
 
-    create ()
+    //load player animations and which frames to use
+    addPlayerAnimations()
     {
-        this.add.image(400,300,'sky');
-
-        let platforms = this.addPlatforms();
-
-        //add player sprite with collison
-        let player = this.physics.add.sprite(100,450,'dude');
-        player.setBounce(0.2);
-        player.setCollideWorldBounds(true);
-
-        this.physics.add.collider(player, platforms);
-
-        //load player animations and which frames to use
         this.anims.create({
             key: 'left',
             frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3}),
@@ -66,6 +58,46 @@ export default class Demo extends Phaser.Scene
             frameRate: 10,
             repeat: -1
         });
+    }
+
+    create ()
+    {
+        this.add.image(400,300,'sky');
+
+        this.addPlatforms();
+
+        //add player sprite with collison
+        this.player = this.physics.add.sprite(100,450,'dude');
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.physics.add.collider(this.player, this.platforms);
+
+        this.addPlayerAnimations();
+        
+    }
+    update() {
+        let cursors = this.input.keyboard.createCursorKeys();
+        if (cursors.left.isDown) 
+        {
+            this.player.setVelocityX(-160);
+            this.player.anims.play('left',true);
+        }
+        else if (cursors.right.isDown)
+        {
+            this.player.setVelocityX(160);
+            this.player.anims.play('right', true);
+        }
+        else 
+        {
+            this.player.setVelocityX(0);
+            this.player.anims.play('turn');
+        }
+
+        if (cursors.up.isDown && this.player.body.touching.down)
+        {
+            this.player.setVelocityY(-330);
+        }
     }
 }
 
